1e69bbf79b81febd0b286cae7745ac5ce064c117
vipoll <vipollmail@gmail.com>
2024-08-28 17:27:18 +0400

    many fixes for android

--- src/mobileapplication3/ui/Keys.java
+++ src/mobileapplication3/ui/Keys.java
@@ -2,17 +2,13 @@ package mobileapplication3.ui;
 
 public class Keys {
 	public static final int
-		SOFT_LEFT = -6,
-	    SOFT_RIGHT = -7,
-		UP = 1,
-		DOWN = 6,
-		LEFT = 2,
-		RIGHT = 5,
-		FIRE = 8,
-		GAME_A = 9,
-		GAME_B = 10,
-		GAME_C = 11,
-		GAME_D = 12,
+		KEY_SOFT_LEFT = -6,
+	    KEY_SOFT_RIGHT = -7,
+		KEY_UP = -1,
+		KEY_DOWN = -2,
+		KEY_LEFT = -3,
+		KEY_RIGHT = -4,
+		KEY_CENTER = -5,
 		KEY_NUM0 = 48,
 		KEY_NUM1 = 49,
 		KEY_NUM2 = 50,
@@ -24,5 +20,14 @@ public class Keys {
 		KEY_NUM8 = 56,
 		KEY_NUM9 = 57,
 		KEY_STAR = 42,
-		KEY_POUND = 35;
+		KEY_POUND = 35,
+		UP = 1,
+		DOWN = 6,
+		LEFT = 2,
+		RIGHT = 5,
+		FIRE = 8,
+		GAME_A = 9,
+		GAME_B = 10,
+		GAME_C = 11,
+		GAME_D = 12;
 }
