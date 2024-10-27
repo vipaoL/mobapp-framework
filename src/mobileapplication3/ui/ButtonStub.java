acf5e8a3d42d7729885a9cbd2ef962cf90ef06de
vipoll <vipollmail@gmail.com>
2024-10-27 23:56:16 +0400

    add ButtonStub. It can be used to make a gap in a ButtonCol
    just a transparent button without a press action. the keyboard cursor jumps over it

---
+++ src/mobileapplication3/ui/ButtonStub.java
@@ -0,0 +1,12 @@
+package mobileapplication3.ui;
+
+public class ButtonStub extends Button {
+
+	public ButtonStub() {
+		super("");
+		setBgColor(IUIComponent.COLOR_TRANSPARENT);
+	}
+
+	public void buttonPressed() { }
+
+}
