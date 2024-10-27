f07d1519c1bb24c2b261f3dea36e49895d6e7cb6
vipoll <vipollmail@gmail.com>
2024-10-27 23:21:31 +0400

    AnimationThread: kinetic scrolling

--- src/mobileapplication3/ui/AnimationThread.java
+++ src/mobileapplication3/ui/AnimationThread.java
@@ -5,6 +5,9 @@
  */
 package mobileapplication3.ui;
 
+import mobileapplication3.platform.Logger;
+import mobileapplication3.platform.Mathh;
+
 /**
  *
  * @author vipaol
@@ -18,7 +21,9 @@ public class AnimationThread implements Runnable {
     private AnimationWorker feedback;
     private boolean isRunning = false;
     private int framesCount;
+    private int frameTime;
     private int x, y, targetX, targetY;
+    private int leftLimitX, rightLimitX, topLimitY, bottomLimitY;
     private int vX = 0, vY = 0;
     private int aX, aY;
     
@@ -26,17 +31,34 @@ public class AnimationThread implements Runnable {
         this.feedback = feedback;
     }
     
-    public void animate(int currX, int currY, int targetX, int targetY, int durationMillis) {
+    public void animate(int currX, int currY, int targetX, int targetY, int durationMillis, int leftLimitX, int rightLimitX, int topLimitY, int bottomLimitY) {
+    	animate(currX, currY, targetX, targetY, durationMillis, leftLimitX, rightLimitX, topLimitY, bottomLimitY, FRAME_MILLIS);
+    }
+    
+    public void animate(int currX, int currY, int targetX, int targetY, int durationMillis, int leftLimitX, int rightLimitX, int topLimitY, int bottomLimitY, int frameTime) {
+    	Logger.log("dx:"+(targetX-currX)+" dy:"+(targetY-currY)+" t:"+durationMillis+" dt:" + frameTime);
         if (currY == targetY && currX == targetX) {
             return;
         }
-        framesCount = FPS * durationMillis / 1000;
+
+        this.frameTime = frameTime;
+
+        this.leftLimitX = leftLimitX * FP_MATH_MULTIPLIER;
+        this.rightLimitX = rightLimitX * FP_MATH_MULTIPLIER;
+        this.topLimitY = topLimitY * FP_MATH_MULTIPLIER;
+        this.bottomLimitY = bottomLimitY * FP_MATH_MULTIPLIER;
         
         x = currX * FP_MATH_MULTIPLIER;
         y = currY * FP_MATH_MULTIPLIER;
         this.targetX = targetX * FP_MATH_MULTIPLIER;
         this.targetY = targetY * FP_MATH_MULTIPLIER;
-        
+
+        framesCount = FPS * durationMillis / 1000;
+        if (framesCount == 0 || frameTime == 0) {
+        	onStep(this.targetX, this.targetY);
+        	return;
+        }
+
         int sX = this.targetX - x;
         int sY = this.targetY - y;
         
@@ -45,10 +67,32 @@ public class AnimationThread implements Runnable {
         
         aX = 2*sX/(framesCount*framesCount);
         aY = 2*sY/(framesCount*framesCount);
+
+        int vX = this.vX;
+        int vY = this.vY;
+        int x = this.x;
+        int y = this.y;
+        for (int i = 0; i < framesCount; i++) {
+            vX -= aX;
+            vY -= aY;
+            x += vX;
+            y += vY;
+        }
+        this.x += this.targetX - x;
+        this.y += this.targetY - y;
+
+        this.targetX = Mathh.constrain(this.leftLimitX, this.targetX, this.rightLimitX);
+        this.targetY = Mathh.constrain(this.topLimitY, this.targetY, this.bottomLimitY);
         
         if (!isRunning) {
             thread = new Thread(this);
             thread.start();
+        }
+    }
+
+    public void stop() {
+        if (thread != null) {
+            thread.interrupt();
         }
     }
 
@@ -62,16 +106,27 @@ public class AnimationThread implements Runnable {
             x += vX;
             y += vY;
             
-            feedback.onStep(x / FP_MATH_MULTIPLIER, y / FP_MATH_MULTIPLIER);
+            onStep(x, y);
+
+            if (!Mathh.nonStrictIneq(leftLimitX, x, rightLimitX) || !Mathh.nonStrictIneq(topLimitY, y, bottomLimitY)) {
+                break;
+            }
             
             try {
-                Thread.sleep(Math.max(0, FRAME_MILLIS - (System.currentTimeMillis() - iterationStartMillis)));
+                Thread.sleep(Math.max(0, frameTime - (System.currentTimeMillis() - iterationStartMillis)));
             } catch (InterruptedException ex) {
-                ex.printStackTrace();
+                isRunning = false;
+                return;
             }
         }
-        feedback.onStep(targetX / FP_MATH_MULTIPLIER, targetY / FP_MATH_MULTIPLIER);
+        onStep(targetX, targetY);
         isRunning = false;
+    }
+
+    private void onStep(int xFP, int yFP) {
+        xFP = Mathh.constrain(leftLimitX, xFP, rightLimitX);
+        yFP = Mathh.constrain(topLimitY, yFP, bottomLimitY);
+        feedback.onStep(xFP / FP_MATH_MULTIPLIER, yFP / FP_MATH_MULTIPLIER);
     }
 
     public interface AnimationWorker {
