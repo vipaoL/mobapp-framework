0e69d3c73a033f767cf007021708ee67d6490723
vipoll <vipollmail@gmail.com>
2025-03-30 02:32:12 +0400

    fixup! fix back/exit button was always displayed as a back button

--- src/mobileapplication3/ui/BackButton.java
+++ src/mobileapplication3/ui/BackButton.java
@@ -1,15 +1,15 @@
 package mobileapplication3.ui;
 
-import mobileapplication3.platform.ui.RootContainerView;
+import mobileapplication3.platform.ui.RootContainer;
 
 public class BackButton extends Button {
 	IPopupFeedback windowParent;
 
 	public BackButton(IPopupFeedback windowParent) {
-		super(windowParent instanceof RootContainerView ? "Exit" : "Back");
+		super(!(windowParent instanceof IUIComponent) ? "Exit" : "Back");
 		this.windowParent = windowParent;
 		setBindedKeyCodes(new int[]{Keys.KEY_NUM0});
-		if (windowParent instanceof RootContainerView) {
+		if (!(windowParent instanceof IUIComponent)) {
 			setBgColor(IUIComponent.BG_COLOR_DANGER);
 		}
 	}
