617db8deeaaf0aca893d17adb348daa9b0bacb3c
vipoll <vipollmail@gmail.com>
2026-03-29 17:12:36 +0400

    GraphicsUtils: add HSVToRGB() and RGBToHSV()

--- src/mobileapplication3/ui/GraphicsUtils.java
+++ src/mobileapplication3/ui/GraphicsUtils.java
@@ -49,4 +49,88 @@ public class GraphicsUtils {
 
         return (0.299 * r) + (0.587 * g) + (0.114 * b);
     }
+
+    public static void RGBToHSV(int color, float[] hsv) {
+        float r = getColorRedComponent(color) / 255.0f;
+        float g = getColorGreenComponent(color) / 255.0f;
+        float b = getColorBlueComponent(color) / 255.0f;
+
+        float max = Math.max(r, Math.max(g, b));
+        float min = Math.min(r, Math.min(g, b));
+        float delta = max - min;
+
+        float h = 0;
+        if (delta != 0) {
+            if (max == r) {
+                h = (g - b) / delta;
+            } else if (max == g) {
+                h = (b - r) / delta + 2f;
+            } else {
+                h = (r - g) / delta + 4f;
+            }
+        }
+        h *= 60;
+        if (h < 0) {
+            h += 360;
+        }
+
+        hsv[0] = h;
+        hsv[1] = (max == 0) ? 0 : (delta / max);
+        hsv[2] = max;
+    }
+
+    public static int HSVToRGB(float h, float s, float v) {
+        while (h < 0) {
+            h += 360;
+        }
+        h = h % 360;
+
+        float c = v * s;
+        float x = c * (1f - Math.abs((h / 60f) % 2f - 1f));
+        float m = v - c;
+
+        float r1 = 0;
+        float g1 = 0;
+        float b1 = 0;
+
+        if (h < 60) {
+            r1 = c;
+            g1 = x;
+            b1 = 0;
+        } else if (h < 120) {
+            r1 = x;
+            g1 = c;
+            b1 = 0;
+        } else if (h < 180) {
+            r1 = 0;
+            g1 = c;
+            b1 = x;
+        } else if (h < 240) {
+            r1 = 0;
+            g1 = x;
+            b1 = c;
+        } else if (h < 300) {
+            r1 = x;
+            g1 = 0;
+            b1 = c;
+        } else {
+            r1 = c;
+            g1 = 0;
+            b1 = x;
+        }
+
+        int r = (int) ((r1 + m) * 255f + 0.5f);
+        int g = (int) ((g1 + m) * 255f + 0.5f);
+        int b = (int) ((b1 + m) * 255f + 0.5f);
+
+        return (Mathh.constrain(0, r, 255) << 16) |
+                (Mathh.constrain(0, g, 255) << 8) |
+                Mathh.constrain(0, b, 255);
+    }
+
+    public static int shiftHue(int color, float degrees) {
+        float[] hsv = new float[3];
+        RGBToHSV(color, hsv);
+        return HSVToRGB(hsv[0] + degrees, hsv[1], hsv[2]);
+    }
 }
