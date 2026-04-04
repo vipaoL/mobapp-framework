48665546c37a3a917a889d00244c43f21c2c5c2c
vipoll <vipollmail@gmail.com>
2026-04-04 14:53:05 +0400

    Switch: add vertical layout

--- src/mobileapplication3/ui/Switch.java
+++ src/mobileapplication3/ui/Switch.java
@@ -13,29 +13,49 @@ public abstract class Switch extends Button {
     protected void drawText(Graphics g, String text, int x0, int y0, int w, int h, boolean isSelected, boolean isFocused, boolean forceInactive, boolean showKbHints) {
         int switchH = font.getHeight();
         int switchW = switchH * 9 / 5;
-        int switchCenterX = x0 + w - Math.min((h - switchH) / 2, switchH) - switchW / 2;
-        int textSwitchSepW = Math.max(switchH, getBgPadding());
 
-        // text
-        int textAreaW = switchCenterX - switchW / 2 - textSwitchSepW - x0;
-        int textCenterOffset = (w - textAreaW) / 2;
-        if (textAreaW < switchW * 4) {
+        int outlineThickness = Math.max(1, switchH / 20);
+        int outlineW = switchW + outlineThickness * 2;
+        int outlineH = switchH + outlineThickness * 2;
+        int outlineArcD = Math.min(outlineW, outlineH);
+
+        int padding = Math.min(h - switchH, switchH) / 2;
+
+        boolean verticalLayout = h > w * 0.6;
+
+        int textAreaW, textAreaH, textX, textY, textCenterOffset;
+        int switchX0, switchY0;
+
+        if (verticalLayout) {
+            textAreaW = w - padding * 2;
+            textAreaH = h - outlineH - padding * 3;
+            textX = x0 + padding;
+            textY = y0 + padding;
             textCenterOffset = 0;
-        }
-        super.drawText(g, text, x0, y0, textAreaW, h, textCenterOffset, 0, isSelected, isFocused, forceInactive, showKbHints);
 
-        int switchX0 = switchCenterX - switchW / 2;
-        int switchY0 = y0 + (h - switchH) / 2;
+            switchX0 = x0 + (w - switchW) / 2;
+            switchY0 = y0 + h - outlineH - padding;
+        } else {
+            switchX0 = x0 + w - padding - switchW;
+            switchY0 = y0 + (h - outlineH) / 2;
+
+            textAreaW = switchX0 - padding - x0;
+            textAreaH = h;
+            textX = x0;
+            textY = y0;
+
+            textCenterOffset = (w - textAreaW) / 2;
+            if (textAreaW < switchW * 4) {
+                textCenterOffset = 0;
+            }
+        }
+
+        super.drawText(g, text, textX, textY, textAreaW, textAreaH, textCenterOffset, 0, isSelected, isFocused, forceInactive, showKbHints);
 
         int switchColor = isActive() ? fontColor : fontColorInactive;
 
         boolean value = getValue();
 
-        // outline
-        int outlineThickness = Math.max(1, font.getHeight() / 20);
-        int outlineW = switchW + outlineThickness * 2;
-        int outlineH = switchH + outlineThickness * 2;
-        int outlineArcD = Math.min(outlineW, outlineH);
         g.setColor(switchColor);
         g.fillRoundRect(
                 switchX0 - outlineThickness,
